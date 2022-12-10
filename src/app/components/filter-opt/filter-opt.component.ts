46dc0ef7b4d413a22ed9a46fb34d4026a9aa2d81
WorldFam <mirekas123@gmail.com>
2022-12-10 12:18:05 +0100

    lazy loading with search functionality

--- src/app/components/filter-opt/filter-opt.component.ts
+++ src/app/components/filter-opt/filter-opt.component.ts
@@ -1,13 +1,8 @@
-import {
-  Component,
-  OnInit,
-  Input,
-  ViewChild,
-} from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { MatSelect } from '@angular/material/select';
 import { BehaviorSubject, combineLatest, Observable, of } from 'rxjs';
-import {  debounceTime, map, scan, startWith, switchMap, } from 'rxjs/operators';
+import { debounceTime, map, scan, startWith, switchMap } from 'rxjs/operators';
 import { Filter } from 'src/app/models/filter-infrastructure.model';
 import { FilterParams } from 'src/app/models/filter-params.model';
 
@@ -38,37 +33,8 @@ export class FilterOptComponent implements OnInit {
   options = new BehaviorSubject<FilterParams[]>([]);
   filteredList: FilterParams[] = [];
 
-  pageSize = 10;
-  pageIndex = 0;
-  searchTerm = '';
-
   ngOnInit(): void {
-
-    // this.filteredOptions$ = this.searchControl.valueChanges.pipe( 
-    //   debounceTime(300),
-    //   switchMap(searchTerm => {
-    //     // Filter the options based on the search term
-    //     const options = this.filter.options.filter(option => option.name.toLowerCase().indexOf(searchTerm.toLowerCase()) === 0);
-    //     // Emit the filtered options
-    //     this.options.next(options);
-    //     // Return an observable that emits the filtered options
-    //     return of(options);
-    //   }),
-    //   scan((acc, curr) => [...acc, ...curr], [])
-
-    // );
-
-    this.getNextBatch();
-    this.filteredOptions$ = this.options.asObservable().pipe(
-      scan((acc, curr) => {
-        return [...acc, ...curr];
-      }, []),
-      // map(options => {
-      //   return options.filter(option => 
-      //     option.name.toLowerCase().indexOf(this.searchTerm.toLowerCase()) === 0);
-      // })
-
-    );
+    this.scanPosition()
 
     this.filterControl = this.getFilterValue();
     this.filterControl.valueChanges.subscribe((value: FilterParams[]) => {
@@ -78,47 +44,41 @@ export class FilterOptComponent implements OnInit {
         this.filteredValue = '';
       }
     });
-
-  
   }
-  // @ViewChild('matSelect', { static: false }) matSelect: MatSelect;
 
-  // ngAfterViewInit() {
-  //   this.matSelect.panel.nativeElement.addEventListener('scroll', () => {
-  //     // Check if the user has scrolled to the bottom of the options
-  //     const scrollTop = this.matSelect.panel.nativeElement.scrollTop;
-  //     const scrollHeight = this.matSelect.panel.nativeElement.scrollHeight;
-  //     const panelHeight = this.matSelect.panel.nativeElement.offsetHeight;
-  
-  //     if (scrollTop + panelHeight >= scrollHeight) {
-  //       // Load the next page of options
-  //       this.getNextBatch()
-  //     }
-  //   });
-  // }
+  onSearchChange(searchTerm: string): void {
+    if (searchTerm) {
+      const filteredList = this.filter.options.filter(
+        (filter) =>
+          filter.name.toLowerCase().indexOf(searchTerm.toLowerCase()) === 0
+      );
+      const filteredOptions = filteredList.length
+        ? filteredList
+        : [{ name: 'No Item found', code: 'null' }];
 
-
-
-  onSearchChange(searchValue: string): void {
-    let filterList = this.filter.options.filter(
-      (filter) =>
-        filter.name.toLowerCase().indexOf(searchValue.toLowerCase()) === 0
-    );
-
-    filterList.length
-        ? this.options.next(filterList) 
-        : this.options.next([{ name: 'No Item found', code: 'null' }]);
+      this.filteredOptions$ = of(filteredOptions);
+    } else {
+      this.offset = 0;
+      this.getNextBatch() 
+      this.scanPosition()
     }
-    
+  }
+
+  scanPosition(){
+    this.filteredOptions$ = this.options.pipe(
+      scan((acc, curr) => {
+        return [...acc, ...curr];
+      }, [])
+    );
+  }
 
   getNextBatch() {
-      const result = this.filter.options.slice(
-        this.offset,
-        this.offset + this.limit
-      );
-      this.options.next(result);
-      this.offset += this.limit;
-    
+    const result = this.filter.options.slice(
+      this.offset,
+      this.offset + this.limit
+    );
+    this.options.next(result);
+    this.offset += this.limit;
   }
 
   getFilterValue = () => this.form.get(this.filter.endpoint) as FormControl;
@@ -131,4 +91,3 @@ export class FilterOptComponent implements OnInit {
     this.getNextBatch();
   }
 }
-
